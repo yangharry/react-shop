592b3a60c528a9bb3bda70bd0ebc1697316b1887
yangharry <myid7771@naver.com>
2023-11-06 21:20:59 +0900

    change homepage from package.json

--- src/api/api-util.js
+++ src/api/api-util.js
@@ -1,12 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, push, get, onValue } from 'firebase/database';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const app = initializeApp({
   apiKey: 'AIzaSyCb-3GUO9b3ea-tXLMFLwYDH4AxGgC4euc',
   databaseURL: 'https://react-shop-ad0b1-default-rtdb.firebaseio.com/',
 });
-const db = getDatabase(app);
 const auth = getAuth(app);
 
 export async function signIn(email, password) {
